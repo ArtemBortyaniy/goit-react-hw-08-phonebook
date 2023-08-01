ce0da1115b111f1a6e5110763e3f1db1903ebec6
Artem <artembariga5@gmail.com>
2023-08-01 20:58:14 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,6 @@
 import AuthNav from 'components/AuthNav/AuthNav';
 import Navigation from 'components/Navigation/Navigation';
-import UserMenu from 'components/UserMenu/UserMenu';
+// import UserMenu from 'components/UserMenu/UserMenu';
 import css from './AppBar.module.css';
 
 const AppBar = () => {
